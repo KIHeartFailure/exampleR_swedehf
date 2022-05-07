38f326b4564ac4f0e126d2e9d08b00cd0f71ef31
Lina Benson <lina.benson@ki.se>
2022-05-07 14:53:21 +0200

    mod text

--- pinteraction_cox_mi.R
+++ pinteraction_cox_mi.R
@@ -9,14 +9,14 @@ library("mice")
 library("survival")
 library("dplyr")
 
-## model with interaction between sex and ef
+# model with interaction between sex and ef
 mod <- with(rsdataimp, coxph(Surv(sos_outtime_death, sos_out_death == "Yes") ~ shf_sex * shf_ef + shf_age))
 
-## Interaction between factors with 2 levels or continuous variables possible to take p for interaction straight from output.
+# Interaction between factors with 2 levels or continuous variables possible to take p for interaction straight from output.
 smod <- summary(pool(mod))
 
-## Interaction between factors with > 2 levels
-## model without interaction between sex and ef
+# Interaction between factors with > 2 levels fit model without interaction between sex and ef
 mod_noint <- with(rsdataimp, coxph(Surv(sos_outtime_death, sos_out_death == "Yes") ~ shf_sex + shf_ef + shf_age))
 
+# and compare
 pint <- mice::D1(mod, mod_noint)
