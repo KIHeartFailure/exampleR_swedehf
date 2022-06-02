1b289ead9753a60e7f7f0b0d43ec9988073e7923
Lina Benson <lina.benson@ki.se>
2022-06-02 16:26:45 +0200

    packs

---
+++ good_packages.R
@@ -0,0 +1,40 @@
+
+install.packages("styler") # for formatting code
+
+# writing
+install.packages("ProjectTemplate")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("kableExtra")
+
+# data handling
+install.packages("purrr")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("tibble")
+install.packages("stringr")
+install.packages("lubridate")
+
+# importing, exporting
+install.packages("haven")
+install.packages("sjlabelled")
+install.packages("openxlsx")
+
+# multiple imputation including parallell computing
+install.packages("mice")
+install.packages("parallel")
+install.packages("doParallel")
+install.packages("foreach")
+
+# presentation and models
+install.packages("tableone")
+install.packages("survival")
+install.packages("MASS")
+install.packages("Matching") # match for propensity scores
+install.packages("coxme") # frailty model for cox, can also do with coxph
+install.packages("splines")
+install.packages("epitools")
+install.packages("car") # for Anova, type 3
+install.packages("cmprsk") # competing rsk
+install.packages("frailtypack")
+install.packages("EValue")
